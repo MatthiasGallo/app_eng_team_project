94974abc3918f6ac213234f1595f5bc804dfdcc8
tbrighentimoretti <tobias.21@hotmail.de>
2020-05-15 09:37:48 +0200

    app folder

---
+++ app/config/config.php
@@ -0,0 +1,4 @@
+<?php
+
+$config = ['MODEL_PATH' => APPLICATION_PATH . DS . 'model' . DS, 'VIEW_PATH' => APPLICATION_PATH . DS . 'view' . DS, 'CONTROLLER_PATH' => APPLICATION_PATH . DS . 'controller' . DS, 'LIB_PATH' => APPLICATION_PATH . DS . 'lib' . DS];
+
