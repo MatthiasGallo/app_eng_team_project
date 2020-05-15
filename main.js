bb6727ef0981a45e03b815b53eb415785ada9e9d
tbrighentimoretti <64213813+tbrighentimoretti@users.noreply.github.com>
2020-05-15 09:49:36 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+$('#open-menu').click(function(){
+
+    $('#menu-page').css('display', 'block');
+
+    $('#open-menu').css('display', 'none');
+
+    $('#close-menu').css('display', 'block');
+
+});
+
+$('#close-menu').click(function(){
+
+    $('#menu-page').css('display', 'none');
+
+    $('#open-menu').css('display', 'block');
+
+    $('#close-menu').css('display', 'none');
+
+});
+
+});
+
+
