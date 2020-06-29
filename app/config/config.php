e93db45bd1d3e16110f8ce589462a3b6b023e92b
tbrighentimoretti <64213813+tbrighentimoretti@users.noreply.github.com>
2020-06-29 15:39:25 +0200

    Delete config.php

--- app/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$config = ['MODEL_PATH' => APPLICATION_PATH . DS . 'model' . DS, 'VIEW_PATH' => APPLICATION_PATH . DS . 'view' . DS, 'CONTROLLER_PATH' => APPLICATION_PATH . DS . 'controller' . DS, 'LIB_PATH' => APPLICATION_PATH . DS . 'lib' . DS];
-
